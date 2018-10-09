e9b2d3f547bf99225e847dc531d418cf89337bf1
igorzakhar <igorzakhar@gmail.com>
2018-10-09 09:58:57 +0300

    Add the SO_REUSEADDR socket option

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import socket
 
 def run():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(('localhost', 5000))
     server_socket.listen()
 
